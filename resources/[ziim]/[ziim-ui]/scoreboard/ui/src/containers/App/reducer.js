6f6730ec4d75885c548cc133be813d799ae918c5
Alex Tedesco <alextede8899@gmail.com>
2021-06-13 09:36:24 -0400

    autoCommit Sun 06/13/2021- 9:36:24.26

--- resources/[ziim]/[ziim-ui]/scoreboard/ui/src/containers/App/reducer.js
+++ resources/[ziim]/[ziim-ui]/scoreboard/ui/src/containers/App/reducer.js
@@ -19,7 +19,7 @@ const scoreboardData = (
     case 'scoreboardShow':
       return { ...state, show: data.payload.show, players: data.payload.players };
     case 'scoreboardUpdate':
-      return { ...state, move: data.payload.move };
+      return { move: data.payload.move };
     default:
       return state;
   }
