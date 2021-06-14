5a82efa4cc210122dcefc23cabe39d3d7c05cff2
Alex Tedesco <alextede8899@gmail.com>
2021-06-14 09:20:05 -0400

    autoCommit Mon 06/14/2021- 9:20:05.23

--- resources/[ziim]/[ziim-ui]/scoreboard/cl_scoreboard.lua
+++ resources/[ziim]/[ziim-ui]/scoreboard/cl_scoreboard.lua
@@ -74,10 +74,10 @@ Citizen.CreateThread(function()
                 -- Logger:Trace("scoreboard", ("Coords: %s"):format(tostring(targetPedCords)))
                 if NetworkIsPlayerTalking(id) then
                     
-                    Utils:DrawText3D(targetPedCords, GetPlayerServerId(id), 247,124,24)
+                    Utils:DrawText3D(targetPedCords.x, targetPedCords.y, targetPedCords.z, GetPlayerServerId(id), 247,124,24)
                     DrawMarker(27, targetPedCords.x, targetPedCords.y, targetPedCords.z-0.97, 0, 0, 0, 0, 0, 0, 1.001, 1.0001, 0.5001, 173, 216, 230, 100, 0, 0, 0, 0)
                 else
-                    Utils:DrawText3D(targetPedCords, GetPlayerServerId(id), 255,255,255)
+                    Utils:DrawText3D(targetPedCords.x, targetPedCords.y, targetPedCords.z, GetPlayerServerId(id), 255,255,255)
                 end
             end
         end
