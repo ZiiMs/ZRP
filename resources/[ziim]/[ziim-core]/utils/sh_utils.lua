0236980614791bfade6a2e23fae09149838d70c8
Alex Tedesco <alextede8899@gmail.com>
2021-06-14 08:57:49 -0400

    autoCommit Mon 06/14/2021- 8:57:49.07

--- resources/[ziim]/[ziim-core]/utils/sh_utils.lua
+++ resources/[ziim]/[ziim-core]/utils/sh_utils.lua
@@ -15,5 +15,35 @@ AddEventHandler("Core:Shared:Ready", function()
 end)
 
 AddEventHandler("Proxy:Shared:RegisterReady", function()
-  exports['zrp-base']:RegisterComponent("Commands", Commands)
+  exports['zrp-base']:RegisterComponent("Utils", Utils)
 end)
+
+Utils = {
+  DrawText3D = function(position, text, r,g,b) 
+    local onScreen,_x,_y=World3dToScreen2d(position.x,position.y,position.z+1)
+    local dist = #(GetGameplayCamCoords()-position)
+ 
+    local scale = (1/dist)*2
+    local fov = (1/GetGameplayCamFov())*100
+    local scale = scale*fov
+   
+    if onScreen then
+        if not useCustomScale then
+            SetTextScale(0.0*scale, 0.55*scale)
+        else 
+            SetTextScale(0.0*scale, customScale)
+        end
+        SetTextFont(0)
+        SetTextProportional(1)
+        SetTextColour(r, g, b, 255)
+        SetTextDropshadow(0, 0, 0, 0, 255)
+        SetTextEdge(2, 0, 0, 0, 150)
+        SetTextDropShadow()
+        SetTextOutline()
+        SetTextEntry("STRING")
+        SetTextCentre(1)
+        AddTextComponentString(text)
+        DrawText(_x,_y)
+    end
+  end,
+}
