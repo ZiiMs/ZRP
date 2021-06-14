008f4e349f94654346f00ffb1370c89dac8925a1
Alex Tedesco <alextede8899@gmail.com>
2021-06-14 09:16:25 -0400

    autoCommit Mon 06/14/2021- 9:16:25.58

--- resources/[ziim]/[ziim-ui]/scoreboard/cl_scoreboard.lua
+++ resources/[ziim]/[ziim-ui]/scoreboard/cl_scoreboard.lua
@@ -71,8 +71,9 @@ Citizen.CreateThread(function()
         if distances[id] then
             if distances[id] < 5 then
                 local targetPedCords = GetEntityCoords(targetPed)
+                Logger:Trace("scoreboard", ("Coords: %s"):format(tostring(targetPedCords)))
                 if NetworkIsPlayerTalking(id) then
-                    Logger:Trace("scoreboard", ("Coords: %s"):format(tostring(targetPedCords)))
+                    
                     Utils:DrawText3D(targetPedCords, GetPlayerServerId(id), 247,124,24)
                     DrawMarker(27, targetPedCords.x, targetPedCords.y, targetPedCords.z-0.97, 0, 0, 0, 0, 0, 0, 1.001, 1.0001, 0.5001, 173, 216, 230, 100, 0, 0, 0, 0)
                 else
