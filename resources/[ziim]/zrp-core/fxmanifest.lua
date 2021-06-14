d2597022448400d50edf4cd5aca9fda7c4a94384
Alex Tedesco <alextede8899@gmail.com>
2021-06-14 09:54:12 -0400

    autoCommit Mon 06/14/2021- 9:54:11.84

--- resources/[ziim]/zrp-core/fxmanifest.lua
+++ resources/[ziim]/zrp-core/fxmanifest.lua
@@ -4,6 +4,11 @@ game 'gta5'
 author 'ZiiM'
 version '1.0.0'
 
+
+-- Base
 client_script 'zrp-base/cl_base.lua'
 shared_script 'zrp-base/sh_base.lua'
 server_script 'zrp-base/sv_base.lua'
+
+--Callbacks
+
