7d72f43f80c2a0d28b04fddd12656f41a5dcd917
Alex Tedesco <alextede8899@gmail.com>
2021-06-14 09:53:49 -0400

    autoCommit Mon 06/14/2021- 9:53:49.15

--- resources/[ziim]/[ziim-core]/zrp-base/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'ZiiM'
-version '1.0.0'
-
-client_script 'cl_base.lua'
-shared_script 'sh_base.lua'
-server_script 'sv_base.lua'
---
+++ resources/[ziim]/zrp-core/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'ZiiM'
+version '1.0.0'
+
+client_script 'zrp-base/cl_base.lua'
+shared_script 'zrp-base/sh_base.lua'
+server_script 'zrp-base/sv_base.lua'
