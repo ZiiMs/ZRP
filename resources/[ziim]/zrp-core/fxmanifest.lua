c2e7966ffcc58a158f753105c7567045ce988d6d
Alex Tedesco <alextede8899@gmail.com>
2021-06-14 09:54:45 -0400

    autoCommit Mon 06/14/2021- 9:54:44.69

--- resources/[ziim]/zrp-core/fxmanifest.lua
+++ resources/[ziim]/zrp-core/fxmanifest.lua
@@ -6,9 +6,11 @@ version '1.0.0'
 
 
 -- Base
-client_script 'zrp-base/cl_base.lua'
-shared_script 'zrp-base/sh_base.lua'
-server_script 'zrp-base/sv_base.lua'
+client_script 'base/cl_base.lua'
+shared_script 'base/sh_base.lua'
+server_script 'base/sv_base.lua'
 
 --Callbacks
 
+client_script 'callbacks/cl_callbacks.lua'
+server_script 'callbacks/sv_callbacks.lua'
