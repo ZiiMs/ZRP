529b4acddf21e4e2efd24ea2d5e213847ad676bf
Alex Tedesco <alextede8899@gmail.com>
2021-06-18 12:35:51 -0400

    autoCommit Fri 06/18/2021-12:35:51.33

--- resources/[ziim]/zrp-database/index.js
+++ resources/[ziim]/zrp-database/index.js
@@ -219,7 +219,7 @@ const Database = {
   * @param {Object} params.options - Options passed to insert.
   * @param {number} params.limit - Limit documents count.
   */
-    findOne: function(self, params, callback) {
+     findOne: function(self, params, callback) {
       if (!checkDatabaseReady()) return;
       if (!checkParams(params)) return Logger.Error(self, ` Database.find: Invalid params object.`);
   
@@ -229,24 +229,16 @@ const Database = {
       const query = safeObjectArgument(params.query);
       const options = safeObjectArgument(params.options);
   
-      let cursor = collection.findOne(query, options).then((document) => {
-        console.log(document)
-        document.toArray((err, doc) => {
-          console.log("Doc: ", doc)
-        })
+      let cursor = collection.find(query, options);
+      if (params.limit) cursor = cursor.limit(params.limit);
+      cursor.toArray((err, documents) => {
+          if (err) {
+              Logger.Error(self, ` Database.find: Error "${err.message}".`);
+              safeCallback(callback, false, err.message);
+              return;
+          };
+          safeCallback(callback, true, exportDocuments(documents));
       });
-      // // console.log(cursor)
-      // for (const i in cursor) {
-      //   console.log(`${i}: ${cursor}`)
-      // }
-      // cursor.toArray((err, documents) => {
-      //     if (err) {
-      //         Logger.Error(self, ` Database.find: Error "${err.message}".`);
-      //         safeCallback(callback, false, err.message);
-      //         return;
-      //     };
-      //     safeCallback(callback, true, exportDocuments(documents));
-      // });
       process._tickCallback();
     },
 
