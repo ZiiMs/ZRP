41fb8a7f842fbf905965392d18acc31e7765d3bf
Alex Tedesco <alextede8899@gmail.com>
2021-06-18 07:19:35 -0400

    autoCommit Fri 06/18/2021- 7:19:35.26

--- resources/[ziim]/zrp-database/fxmanifest.lua
+++ resources/[ziim]/zrp-database/fxmanifest.lua
@@ -4,12 +4,6 @@ game 'gta5'
 author 'ZiiM'
 version '1.0.0'
 
-ui_page 'ui/build/index.html'
+server_only 'yes'
 
-files {
-  'ui/build/index.html',
-  'ui/build/main.js'
-}
-
-client_script 'cl_scoreboard.lua'
-server_script 'sv_scoreboard.lua'
+server_script 'index.js'
