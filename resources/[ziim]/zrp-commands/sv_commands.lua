72adacf15fe05a85a99a5ab0d95bef1755089b7d
Alex Tedesco <alextede8899@gmail.com>
2021-06-18 10:25:40 -0400

    autoCommit Fri 06/18/2021-10:25:40.30

--- resources/[ziim]/zrp-commands/sv_commands.lua
+++ resources/[ziim]/zrp-commands/sv_commands.lua
@@ -49,11 +49,40 @@ Database:insert({ collection="Players", documents = {{ username = "Test", passwo
 end)
 end, false)
 
-
 Database:createIndex({ collection="Players", query = { username = 1 }, options = { unique = true } }, function (success, result)
   if not success then
     print("[MongoDB][Example] Error in insertOne: "..tostring(result))
     return
   end
   print("[MongoDB][Example] Index created " ..tostring(result).."!")
+end)
+
+Database:count({collection="Players"}, function(success, result)
+  if not success then
+    print("[MongoDB][Example] Error in count: "..tostring(result))
+    return
+  end
+  print("[MongoDB][Example] Current users count: "..tostring(result))
+end)
+
+local params = {
+  collection = "Players",
+  query = {},
+  limit = 5,
+  options = {
+      -- Include username and exclude _id field
+      projection = {username = 1, _id = 0}
+  }
+}
+Database:find(params, function (success, result)
+  if not success then
+      return
+  end
+
+  print("\n** 5 users")
+  for i, document in ipairs(result) do
+      for k, v in pairs(document) do
+          print("* "..tostring(k).." = \""..tostring(v).."\"")
+      end
+  end
 end)
