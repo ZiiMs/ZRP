029ed4ce77221361df24f19e06681bfd3e08859b
Alex Tedesco <alextede8899@gmail.com>
2021-06-18 16:18:06 -0400

    autoCommit Fri 06/18/2021-16:18:05.78

--- resources/[ziim]/zrp-commands/sv_commands.lua
+++ resources/[ziim]/zrp-commands/sv_commands.lua
@@ -67,7 +67,7 @@ end, false)
 
 RegisterCommand('tetu', function(source, args)
   -- TODO: make a vehicle! fun!
-  -- local username = args[1] or "Test"
+  local username = args[1] or "WERRwerTest"
   Database:findOneAndUpdate({ collection="Players", query = { username = username }, update = { ["$set"] = { first_name = "Fierell" }}}, function (success, result)
     if not success then
         print("[MongoDB][Example] Error in findOneAndUpdate: "..tostring(result))
