0efeb01f6614e95fd51af8c1327207ced2bb223e
Alex Tedesco <alextede8899@gmail.com>
2021-06-20 03:58:04 -0400

    autoCommit Sun 06/20/2021- 3:58:04.25

--- resources/[ziim]/zrp-core/players/sv_players.lua
+++ resources/[ziim]/zrp-core/players/sv_players.lua
@@ -71,7 +71,7 @@ Players = {
 
     user.source = src
     user.name = GetPlayerName(src)
-    user.steamid = self:GetIdent(src, "steamid")
+    user.steamid = self:GetIdent(src, "steam")
     user.license = self:GetIdent(src, "license")
     user.ip = GetPlayerEndpoint(src)
     user.rank = "user"
