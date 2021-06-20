59a6b0aae71402f957adb8a14d1e9594966d7503
Alex Tedesco <alextede8899@gmail.com>
2021-06-20 04:05:01 -0400

    autoCommit Sun 06/20/2021- 4:05:01.24

--- resources/[ziim]/zrp-core/core/sv_characters.lua
+++ resources/[ziim]/zrp-core/core/sv_characters.lua
@@ -56,7 +56,7 @@ Core = {
           end
           print("[MongoDB][Example] User created. New ID: "..tostring(insertedIds[1]))
         end)
-        callback(result)
+        callback(result.ops)
       end
     end)
 
