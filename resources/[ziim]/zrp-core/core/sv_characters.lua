7ffd41df1417efefcfc1c42282feb3dff86a9768
Alex Tedesco <alextede8899@gmail.com>
2021-06-20 03:06:42 -0400

    autoCommit Sun 06/20/2021- 3:06:41.88

---
+++ resources/[ziim]/zrp-core/core/sv_characters.lua
@@ -0,0 +1,20 @@
+function RetrieveComponents()
+  Logger = exports['zrp-core']:FetchComponent('Logger')
+end
+
+
+AddEventHandler("Core:Shared:Ready", function()
+  exports['zrp-core']:RequestDependencies('Base', {
+    'Logger'
+  }, function(error)
+    if #error > 0 then
+      return
+    end
+    RetrieveComponents()
+  end)
+end)
+
+AddEventHandler("Proxy:Shared:RegisterReady", function()
+	print("Working?")
+  exports['zrp-core']:RegisterComponent("Core", Core)
+end)
