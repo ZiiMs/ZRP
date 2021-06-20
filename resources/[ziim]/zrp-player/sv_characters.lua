83553a924530bf4a607597a53bbd6dfd41d9dc18
Alex Tedesco <alextede8899@gmail.com>
2021-06-20 13:36:22 -0400

    autoCommit Sun 06/20/2021-13:36:22.07

--- resources/[ziim]/zrp-player/sv_characters.lua
+++ resources/[ziim]/zrp-player/sv_characters.lua
@@ -29,42 +29,42 @@ end)
 --   exports['zrp-core']:RegisterComponent("Core", Core)
 -- end)
 
-Core = {
-  LoadPlayer = function(self, src, cb)
-    local user = Players:CreatePlayer(src, false)
+-- Core = {
+--   LoadPlayer = function(self, src, cb)
+--     local user = Players:CreatePlayer(src, false)
 
-    if not user then 
-      user = Players:CreatePlayer(src, false)
-      if not user then print("Error cant create character") return end
-    end
-    Logger:Trace("characters", Database)
+--     if not user then 
+--       user = Players:CreatePlayer(src, false)
+--       if not user then print("Error cant create character") return end
+--     end
+--     Logger:Trace("characters", Database)
 
-    Database:findOne({ collection="Players", query = { steamid = Players:GetIdent(src, "steamid") } }, function (success, result)
-      if not success then
-          print("[MongoDB][Example] Error in findOne: "..tostring(result))
-          return
-      end
-      if result then
-        print("[MongoDB][Example] User is already created " .. tostring(result._id))
-        user:setRank(result.rank)
+--     Database:findOne({ collection="Players", query = { steamid = Players:GetIdent(src, "steamid") } }, function (success, result)
+--       if not success then
+--           print("[MongoDB][Example] Error in findOne: "..tostring(result))
+--           return
+--       end
+--       if result then
+--         print("[MongoDB][Example] User is already created " .. tostring(result._id))
+--         user:setRank(result.rank)
 
-        callback(result)
-      else
-        Database:insertOne({ collection="Players", document = { 
-          name = user.name, 
-          rank = user.rank ,
-          steamid = user.steamid,
-          license = user.license,
-          ip = user.ip,
-        }}, function (success, result, insertedIds)
-          if not success then
-            print("[MongoDB][Example] Error in insertOne: "..tostring(result))
-            return
-          end
-          print("[MongoDB][Example] User created. New ID: "..tostring(insertedIds[1]))
-        end)
-        callback(result)
-      end
-    end)
-  end,
-}
+--         callback(result)
+--       else
+--         Database:insertOne({ collection="Players", document = { 
+--           name = user.name, 
+--           rank = user.rank ,
+--           steamid = user.steamid,
+--           license = user.license,
+--           ip = user.ip,
+--         }}, function (success, result, insertedIds)
+--           if not success then
+--             print("[MongoDB][Example] Error in insertOne: "..tostring(result))
+--             return
+--           end
+--           print("[MongoDB][Example] User created. New ID: "..tostring(insertedIds[1]))
+--         end)
+--         callback(result)
+--       end
+--     end)
+--   end,
+-- }
