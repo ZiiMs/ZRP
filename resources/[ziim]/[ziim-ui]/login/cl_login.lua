d387adfa8948ba852d86545977e3d4b106a704c8
Alex Tedesco <alextede8899@gmail.com>
2021-07-02 13:32:54 -0400

    autoCommit Fri 07/02/2021-13:32:53.67

--- resources/[ziim]/[ziim-ui]/login/cl_login.lua
+++ resources/[ziim]/[ziim-ui]/login/cl_login.lua
@@ -19,7 +19,7 @@ end, false)
 local function Init()
   RegisterNUICallback('FetchData', function(data, cb)
     print("Before CB:!:!")
-    local data = Callbacks:TriggerServerCallbackTimeout("login:FetchData", 25000)
+    local data = Callbacks:TriggerServerCallbackTimeout("login:getData", 25000)
     print("Data?: ", data)
     if data then 
       SendNUIMessage({
